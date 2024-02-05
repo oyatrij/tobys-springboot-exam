b8ea9cbce21f648b737945a364667469740bed38
Jaemin Lee <oyatrij@gmail.com>
2024-02-06 07:51:21 +0900

    사용자 정보 저장용 자바빈 User 클래스

---
+++ src/main/java/tobyspringboot/exam/domain/User.java
@@ -0,0 +1,32 @@
+package tobyspringboot.exam.domain;
+
+public class User {
+    //Javabean 규약을 따르는 오브젝트 이용
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
